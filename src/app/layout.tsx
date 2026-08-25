d0008fc92b883cddf5818b7eb509920465501b80
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-08-25 23:54:30 +0530

    Feat: Add ReduxProvider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/lib/provider";
+import ReduxProvider from "@/redux/ReduxProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><Provider>{children}</Provider></body>
+      <body className="min-h-full flex flex-col">
+        <Provider>
+          <ReduxProvider>
+            {children}
+          </ReduxProvider>
+        </Provider>
+      </body>
     </html>
   );
 }
