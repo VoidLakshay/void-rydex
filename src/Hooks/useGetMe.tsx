146c6a4f1d7926234c94cba9e47f2cc47aa62e5c
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-08-28 23:10:26 +0530

    Fix: Add error handling (try/catch) to getMe API call in useGetMe hook

--- src/Hooks/useGetMe.tsx
+++ src/Hooks/useGetMe.tsx
@@ -4,20 +4,24 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setUserData } from '@/redux/UserSlice'
 
-function useGetMe() {
+function useGetMe(enabled: boolean) {
   const dispatch = useDispatch()
 
   useEffect(() => {
+    if (!enabled) {
+      return
+    }
+
     const getMe = async () => {
         try {
             const { data } = await axios.get("/api/user/me")
             dispatch(setUserData(data))
         } catch (error) {
-            console.error("Error fetching user data:", error)
+            console.log(error)
         }
     }
     getMe()
-  }, [dispatch])
+  }, [enabled])
 }
 
 export default useGetMe
