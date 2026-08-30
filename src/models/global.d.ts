e67c972b29f58ca67a7ab0b7ee7bb1a82d393a79
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-08-30 23:38:45 +0530

    docs: Add JSDoc for global mongoose connection cache

--- src/models/global.d.ts
+++ src/models/global.d.ts
@@ -1,6 +1,9 @@
 import { Connection } from "mongoose";
 
 declare global {
+  /**
+   * Cached Mongoose connection to prevent multiple connections in development.
+   */
   var mongooseConn: {
     conn: Connection | null,
     promise: Promise<Connection> | null,
