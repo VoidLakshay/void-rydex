206ca119ef73658cf37f3e44f80da2eb1aaf0518
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-09-11 23:48:23 +0530

    docs: update schema comment in user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -15,7 +15,7 @@ interface IUser extends mongoose.Document {
   updatedAt: Date;
 }
 
-// Schema definition for the User model
+// Mongoose schema definition for the User model
 const userSchema = new mongoose.Schema<IUser>(
   {
     name: { type: String, required: true },
